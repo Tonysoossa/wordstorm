cbc4c330827c7175d0117f5794bf9ba0f6ce54b1
Tonysoossa <tony.servanin@gmail.com>
2025-01-28 21:03:53 +0100

    first comit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container mx-auto px-4 text-center">
+        &copy; {new Date().getFullYear()} Wordstorm Game by Servanin Tony, All
+        righ reserved.
+      </div>
+    </footer>
+  );
+}
