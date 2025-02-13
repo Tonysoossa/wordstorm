d9b8d6201114c84108beaef082bba3a955e2e507
Tonysoossa <tony.servanin@gmail.com>
2025-02-14 00:08:37 +0100

    mise en place d'un background animé

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { geistMono, doto, silkscreen} from "./assets/fonts";
+import { geistMono, doto, silkscreen } from "./assets/fonts";
+import { BackgroundBeamsWithCollision } from "@/app/components/ui/background-beams-with-collision";
 import Footer from "@/app/containers/Footer";
 import NavBar from "@/app/containers/Nav";
 import "./globals.css";
@@ -15,12 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${doto.variable} ${silkscreen.variable} ${geistMono.variable}`}>
-      <body className="antialiased pr-12 pl-12 pt-2 font-geistMono">
-        <NavBar />
-        <main className="flex-grow text-center container mx-auto px-4 py-20">
-          {children}
-        </main>
+    <html
+      lang="en"
+      className={`${doto.variable} ${silkscreen.variable} ${geistMono.variable} `}
+    >
+      <body className="font-geistMono flex text-center mx-auto flex-grow max-w-[1440px]">
+        <BackgroundBeamsWithCollision>
+          <NavBar />
+          <main className="px-4 py-20">{children}</main>
+        </BackgroundBeamsWithCollision>
         <Footer />
       </body>
     </html>
