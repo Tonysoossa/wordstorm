5c30ee93c4ba0cbff300f1cfea99ae463c6598a4
Tonysoossa <tony.servanin@gmail.com>
2025-03-06 14:48:39 +0100

    fix: re-center the footer

--- app/containers/Footer.tsx
+++ app/containers/Footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="absolute bottom-0 left-0 right-0 z-50 flex justify-center pb-0.5 ml-5 mr-5 max-md:text-sm">
-        &copy; {new Date().getFullYear()} Wordstorm Game by Servanin Tony, All
-        right reserved.
+      <div className="absolute bottom-0 left-0 right-0 z-50 flex justify-center pb-0.5 ml-5 mr-5 max-md:text-sm text-gray-400">
+        <p>
+          &copy; {new Date().getFullYear()} Wordstorm Game by Servanin Tony, All
+          right reserved.
+        </p>
       </div>
     </footer>
   );
