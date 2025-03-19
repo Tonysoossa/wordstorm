8875023d40b24643778f5832d2eab718f3d4b316
Tonysoossa <tony.servanin@gmail.com>
2025-03-19 15:34:39 +0100

    add: content of the main page (buttons)

---
+++ app/containers/Main.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useUser, SignInButton } from "@clerk/nextjs";
+import { useLanguage } from "@/app/assets/languages/LanguagesContext";
+import MainBtn from "../components/buttons/MainBtn";
+
+export default function Main() {
+  const { isSignedIn, user } = useUser();
+  const { translations } = useLanguage();
+
+  return (
+    <section>
+      {!isSignedIn && (
+        <div>
+          <p>
+            Please register to your account or sign up if you&apos;re a new user
+            ! Or play as a guest but scores couldn&apos;t be saved. Have fun !
+          </p>
+          <MainBtn />
+        </div>
+      )}
+      {isSignedIn && (
+        <div>
+          {translations.index.helloUser} {user.username} !
+        </div>
+      )}
+    </section>
+  );
+}
