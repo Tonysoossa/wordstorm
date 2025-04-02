4fd8a5153212d26ee92b5101205361c6ca4195fe
Tonysoossa <tony.servanin@gmail.com>
2025-04-02 15:14:04 +0200

    fix: content center fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { geistMono, silkscreen } from "./assets/fonts";
 import { BackgroundBeamsWithCollision } from "@/app/components/ui/background-beams-with-collision";
 import { LanguageProvider } from "./assets/languages/LanguagesContext";
-
 import Footer from "@/app/containers/Footer";
 import NavBar from "@/app/containers/Nav";
 import "./globals.css";
@@ -28,13 +27,13 @@ export default function RootLayout({
       >
         <LanguageProvider>
           <head></head>
-          <body className="font-geistMono flex text-center mx-auto flex-grow">
+          <body className="font-geistMono flex">
             <BackgroundBeamsWithCollision>
               <NavBar />
-              <main className="px-4 py-40 flex justify-center">{children}</main>
+              <main className="py-28 flex justify-center text-center">{children}</main>
             </BackgroundBeamsWithCollision>
             <Footer />
-              <CharacterCursor />
+            <CharacterCursor />
           </body>
         </LanguageProvider>
       </html>
