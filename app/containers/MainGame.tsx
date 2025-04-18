5b78467eb561e59abd4c3eb3bea6b65150c76181
Tonysoossa <tony.servanin@gmail.com>
2025-04-18 17:26:13 +0200

    add: Main content of the game page created

---
+++ app/containers/MainGame.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+// import { useLanguage } from "@/app/assets/languages/LanguagesContext";
+import capitalize from "../assets/function/firstLetterUpperCase";
+import { SignedOut } from "@clerk/nextjs";
+import StartGameBtn from "../components/buttons/StartGameBtn";
+
+export default function MainGame() {
+  const { isSignedIn, user } = useUser();
+  // const { translations } = useLanguage();
+
+  return (
+    <div className="flex-col flex justify-center items-center gap-y-12 pt-28">
+      <SignedOut>
+        <p>Alright stranger, Let&apos;s start a game !</p>
+      </SignedOut>
+      {isSignedIn && (
+        <p>
+          {user?.username ? capitalize(user.username) : ""}, Let&apos;s start a
+          game !
+        </p>
+      )}
+      <StartGameBtn />
+    </div>
+  );
+}
