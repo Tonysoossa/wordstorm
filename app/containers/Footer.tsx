1ff0e8cf3cf91d921d15bca021b53bc343f0977f
Tonysoossa <tony.servanin@gmail.com>
2025-04-20 01:36:13 +0200

    fix: footer set to fix position

--- app/containers/Footer.tsx
+++ app/containers/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="absolute bottom-0 left-0 right-0 z-50 flex text-center justify-center pb-0.5 ml-5 mr-5 max-md:text-sm text-gray-400 flex-grow-0">
+      <div className="fixed bottom-0 left-0 right-0 z-50 flex text-center justify-center pb-0.5 ml-5 mr-5 max-md:text-sm text-gray-400 flex-grow-0">
         <p>
           &copy; {new Date().getFullYear()} Wordstorm Game by Servanin Tony, All
           right reserved.
