112495215d1318dc2e0db485e376636be9bed608
Tonysoossa <tony.servanin@gmail.com>
2025-04-19 17:44:16 +0200

    update: optimization

--- app/components/buttons/MainBtn.tsx
+++ app/components/buttons/MainBtn.tsx
@@ -8,8 +8,8 @@ export default function MainBtn() {
     <div>
       <SignedOut>
         <div className="flex justify-center pt-20 gap-16 mx-auto ">
-        <Link href="/game">
-            <button className="shadow-[-3px_3px_5px_0.5px_#fff6af] bg-gradient-to-b from-[#ffed64] to-[#ffab23] rounded-3xl cursor-pointer text-black font-silkscreen text-lg w-28 hover:scale-110 hover:hover:shadow-[3px_3px_5px_0.5px_#fff6af] h-10">
+          <Link href="/game">
+            <button className="shadow-[0px_3px_5px_1.5px_#fff6af] bg-gradient-to-b from-[#ffed64] to-[#ffab23] rounded-3xl cursor-pointer text-black font-silkscreen text-lg w-28 hover:scale-110 hover:shadow-[0px_3px_5px_1px_#fff6af] h-10">
               Play !
             </button>
           </Link>
@@ -21,9 +21,10 @@ export default function MainBtn() {
         </div>
       </SignedOut>
       <SignedIn>
-        <div className="flex justify-center gap-12">
+        <div>
+          <p className="pb-12">Maintenant connecter, il est temps de jouer !</p>
           <Link href="/game">
-            <button className="shadow-[-3px_3px_5px_0.5px_#fff6af] bg-gradient-to-b from-[#ffed64] to-[#ffab23] rounded-3xl cursor-pointer text-black font-silkscreen text-lg w-28 hover:scale-110 hover:hover:shadow-[3px_3px_5px_0.5px_#fff6af] h-10">
+            <button className="shadow-[0px_3px_5px_1.5px_#fff6af] bg-gradient-to-b from-[#ffed64] to-[#ffab23] rounded-3xl cursor-pointer text-black font-silkscreen text-lg w-28 hover:scale-110 hover:shadow-[0px_3px_5px_1px_#fff6af] h-10 font-normal">
               Play !
             </button>
           </Link>
