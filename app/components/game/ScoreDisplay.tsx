3e2910e943ed57142cf3d8cb1505c903766eae46
Tonysoossa <tony.servanin@gmail.com>
2025-04-19 23:38:25 +0200

    add: translation added

--- app/components/game/ScoreDisplay.tsx
+++ app/components/game/ScoreDisplay.tsx
@@ -1,16 +1,26 @@
-// components/ScoreDisplay.tsx
 import { useScore } from "../../context/ScoreContext";
 import { useUser } from "@clerk/nextjs";
+import { useLanguage } from "../../assets/languages/LanguagesContext";
 
 export default function ScoreDisplay() {
   const { lastScore, highScore } = useScore();
   const { isSignedIn } = useUser();
+  const { translations } = useLanguage();
 
   return (
-    <div className="py-4 bg-white text-black rounded-lg shadow-xl px-10 z-50">
-      <h2 className="text-2xl font-bold mb-4">Mes Scores</h2>
-      <p className="">Dernier score: {lastScore}</p>
-      {isSignedIn && <p>Meilleur score: {highScore}</p>}
+    <div className="py-4 bg-white text-black rounded-lg shadow-xl px-10 ">
+      <h2 className="text-2xl font-bold mb-4">
+      {translations.displayScore.myScores}
+      </h2>
+      <p>
+        {translations.displayScore.lastScore}
+        {lastScore}
+      </p>
+      {isSignedIn && (
+        <p>
+          {translations.displayScore.highScore} {highScore}
+        </p>
+      )}
     </div>
   );
 }
