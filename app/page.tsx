fe8adc93756ed22c962de1ce605ab796788b801c
Tonysoossa <tony.servanin@gmail.com>
2025-04-22 15:56:24 +0200

    delete: useless div deleted

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Main from "@/app/containers/Main";
 
 export default function Home() {
   return (
-    <section className="flex-col pt-18">
-      <div className="pt-10 max-w-xl text-md">
-        <Main />
-      </div>
+    <section className="flex-col pt-28 max-w-xl text-md">
+      <Main />
     </section>
   );
 }
